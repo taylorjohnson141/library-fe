c38c870f8da4977832b228ecb03128bd81c79ac6
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-20 16:18:35 -0700

    Add BookShelf path

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-
+import {Route} from 'react-router-dom'
+import BookShelf from '../BookShelf/BookShelf'
 function App() {
   let [books, upDateBooks] = useState([])
    useEffect(async () =>{
@@ -13,7 +14,10 @@ function App() {
   })
   return (
     <section>
-          {findBooks}
+         <Route exactpath ='/'
+         render ={()=>{
+          return <BookShelf  books = {books}/>
+         }}></Route>
 
     </section>
        
