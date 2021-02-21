58830c82076d4f91f98ac7d81a634fa88fe67017
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-21 16:12:18 -0700

    Add header to post request

--- src/AddBook/AddBook.js
+++ src/AddBook/AddBook.js
@@ -9,24 +9,36 @@ function AddBook (){
     hasRead:false,
   })
 
-  let onChange = (event) =>{
+  let onChange = async (event) =>{
     if(coverPhoto.current.files.length !== 0){
       addBook({...book,coverPhoto:coverPhoto.current.files[0].name})
-    }else if(event.target.id === 'hasRead'){
+    }
+    if(event.target.id === 'hasRead'){
       let val = false
       if(event.target.value === 'false'){
         val = true
       } 
-      addBook({...book, [event.target.id]: val})
+      console.log('here')
+      //set state async always behind
+        addBook({...book, hasRead: val})
 
     }else{
       addBook({...book, [event.target.id]: event.target.value})
     }
   }
   let onSubmit = (event) =>{
-      fetch('http://localhost:9000/AddPost',{
+    console.log(book)
+    event.preventDefault()
+      fetch('http://localhost:9000/addBook',{
         method: 'Post',
-        body:JSON.stringify(book)
+        body:JSON.stringify(book),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then(respose =>{
+        return respose.json()
+      }).then(data =>{
+        console.log(data)
       })
     
 
@@ -44,13 +56,13 @@ function AddBook (){
       </label>
       <label>
         Cover:
-        <input type="file" htmlFor = 'coverPhoto' id ='coverPhote'  ref ={coverPhoto} onChange = {onChange} required/>
+        <input type="file" htmlFor = 'coverPhoto' id ='coverPhoto'  ref ={coverPhoto} onChange = {onChange} required/>
       </label>
       <label>
         Check if you have Read this book:
         <input type="checkbox" htmlFor = 'hasRead' id ='hasRead'  value = {book.hasRead} onChange = {onChange} required/>
       </label>
-      <button onClick = {onSubmit}>Submit</button>
+      <button onClick = {(event) =>{onSubmit(event)}}>Submit</button>
     </form>
   )
 }
